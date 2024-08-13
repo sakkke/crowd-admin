62d7d26325145fb2bb1965a25a1bffbadce97280
Konosuke Sakai <84666033+sakkke@users.noreply.github.com>
2024-08-14 04:41:02 +0900

    feat: add realtime feature

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,8 +1,25 @@
-import { createClient } from '@/utils/supabase/server';
+"use client";
 
-export default async function Notes() {
-  const supabase = createClient();
-  const { data: notes } = await supabase.from("notes").select();
+import { createClient } from '@/utils/supabase/client';
+import { useEffect, useState } from 'react';
+
+export default function Notes() {
+  const [notes, setNotes] = useState<any>([]);
+  useEffect(() => {
+    const supabase = createClient();
+    supabase.from("notes").select().then(({ data }) => {
+      setNotes(data);
+    });
+
+    const handleInserts = (payload: any) => {
+      setNotes((prevNotes: any) => [...prevNotes, payload.new]);
+    };
+
+    supabase
+      .channel("notes")
+      .on("postgres_changes", { event: "INSERT", schema: "public", table: "notes" }, handleInserts)
+      .subscribe();
+  }, []);
 
   return <pre>{JSON.stringify(notes, null, 2)}</pre>
 }
